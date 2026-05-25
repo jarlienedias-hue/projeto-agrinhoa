0d11bf555d273732504fa21d56163862fc129bfc
jarlienedias-hue <jarliene.dias@escola.pr.gov.br>
2026-05-25 07:56:56 -0300

    Add event listeners for message and contact form

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// script.js
 
+const btnMensagem = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
+
+btnMensagem.addEventListener("click", () => {
+  mensagem.innerHTML =
+    "O agronegócio sustentável combina inovação, produtividade e preservação ambiental.";
+});
+
+const formContato = document.getElementById("formContato");
+const resposta = document.getElementById("resposta");
+
+formContato.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+
+  resposta.innerHTML = `Obrigado pelo contato, ${nome}! Em breve responderemos sua mensagem.`;
+
+  formContato.reset();
+});
